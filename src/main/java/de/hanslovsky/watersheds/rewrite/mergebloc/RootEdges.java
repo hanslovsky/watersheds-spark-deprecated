15705ede2ebfd1f95874bd60ff582b888cbfeef2
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-03-01 10:24:19 -0500

    Add missing file

---
+++ src/main/java/de/hanslovsky/watersheds/rewrite/mergebloc/RootEdges.java
@@ -0,0 +1,43 @@
+package de.hanslovsky.watersheds.rewrite.mergebloc;
+
+import org.apache.spark.api.java.function.Function;
+
+import de.hanslovsky.watersheds.rewrite.graph.Edge;
+import net.imglib2.algorithm.morphology.watershed.DisjointSets;
+import scala.Tuple2;
+
+public class RootEdges implements Function< Tuple2< Long, MergeBlocOut >, Tuple2< Long, MergeBlocOut > >
+{
+
+	@Override
+	public Tuple2< Long, MergeBlocOut > call( final Tuple2< Long, MergeBlocOut > input ) throws Exception
+	{
+		final Edge e = new Edge( input._2().edges );
+		final DisjointSets map = input._2().dj;
+		final long[] counts = input._2().counts;
+		for ( int i = 0; i < e.size(); ++i )
+		{
+			e.setIndex( i );
+			final int from = ( int ) e.from();
+			final int to = ( int ) e.to();
+
+			final int rFrom = map.findRoot( from );
+			final int rTo = map.findRoot( to );
+
+			if ( rFrom != from )
+				counts[ from ] = 0;
+
+			if ( rTo != to )
+				counts[ to ] = 0;
+
+			e.from( rFrom );
+			e.to( rTo );
+
+			if ( rFrom == rTo )
+				e.weight( -1.0 );
+
+		}
+		return input;
+	}
+
+}
